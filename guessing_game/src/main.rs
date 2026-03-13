6775a212f1ff6c6d84995531df56e302b36eec46
kaihere14 <yashpalsharma9950@gmail.com>
2026-03-13 23:46:33 +0530

    feat: Implement a guessing game with random number generation and user input handling.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello doston");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+    println!("Enter your number for guess : ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read");
+    let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid input");
+                continue;
+            },
+        };
+
+    println!("You guessed : {}",guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        },
+    }
+    }
+
 }
